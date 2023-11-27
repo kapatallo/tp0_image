b6174fb33fffdc834767ec4550f109036fc1f713
jawad mecheri <jmecheri01@yahoo.com>
2023-11-27 16:56:57 +0100

    v2

--- sampleCode.cpp
+++ sampleCode.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
 using namespace cv;
 using namespace std;
+
+void calculateHistogram(const Mat& image, Mat& hist, int histSize);
+void stretchHistogram(const Mat& image, Mat& stretchedImage, double minVal, double maxVal);
+void equalizeHistogram(const Mat& image, Mat& equalizedImage);
 
 int main(void) {
     // Load the boy image in GrayScale mode
@@ -20,43 +23,23 @@ int main(void) {
     Mat kernel = (Mat_<float>(3, 3) << 1, 1, 1, 1, -7, 1, 1, 1, 1); // Example filter
 
     // Create a Mat to store the filtered image
-        Mat filteredBoyImage = Mat::zeros(boyImage.size(), boyImage.type());
+    Mat filteredBoyImage = Mat::zeros(boyImage.size(), boyImage.type());
 
-        // Perform convolution
-        for (int y = 1; y < boyImage.rows - 1; y++) {
-            for (int x = 1; x < boyImage.cols - 1; x++) {
-                float sum = 0.0;
-                for (int k = -1; k <= 1; k++) {
-                    for (int j = -1; j <= 1; j++) {
-                        sum += kernel.at<float>(k + 1, j + 1) * boyImage.at<uchar>(y + k, x + j);
-                    }
+    // Perform convolution
+    for (int y = 1; y < boyImage.rows - 1; y++) {
+        for (int x = 1; x < boyImage.cols - 1; x++) {
+            float sum = 0.0;
+            for (int k = -1; k <= 1; k++) {
+                for (int j = -1; j <= 1; j++) {
+                    sum += kernel.at<float>(k + 1, j + 1) * boyImage.at<uchar>(y + k, x + j);
                 }
-                filteredBoyImage.at<uchar>(y, x) = saturate_cast<uchar>(sum);
             }
+            filteredBoyImage.at<uchar>(y, x) = saturate_cast<uchar>(sum);
         }
+    }
 
     // Display the filtered image
     imshow("Filtered Boy Image", filteredBoyImage);
-
-    // Calculate and normalize the histogram for the boy image
-    int histSize = 256; // number of bins
-    float range[] = { 0, 256 };
-    const float* histRange = { range };
-    Mat boyHist;
-    calcHist(&boyImage, 1, 0, Mat(), boyHist, 1, &histSize, &histRange, true, false);
-    normalize(boyHist, boyHist, 0, 400, NORM_MINMAX, -1, Mat());
-
-    // Draw the histogram for the boy image
-    int hist_w = 512; int hist_h = 400;
-    int bin_w = cvRound((double)hist_w / histSize);
-    Mat boyHistImage(hist_h, hist_w, CV_8UC1, Scalar(0));
-    for (int i = 0; i < histSize; i++) {
-        line(boyHistImage,
-            Point(bin_w * i, hist_h),
-            Point(bin_w * i, hist_h - cvRound(boyHist.at<float>(i))),
-            Scalar(255),
-            bin_w);
-    }
 
     // Load the underexposed image in GrayScale mode
     Mat underexposedImage = imread("underexposed.jpg", 0);
@@ -67,30 +50,24 @@ int main(void) {
         return -1;
     }
 
-
-
-    // Find the min and max pixel values and their locations for the underexposed image
+    // Find the min and max pixel values for the underexposed image
     double minVal, maxVal;
     minMaxLoc(underexposedImage, &minVal, &maxVal);
     cout << "Underexposed MinVal: " << minVal << ", MaxVal: " << maxVal << endl;
 
-    // Stretch the histogram of the underexposed image
+    // Stretch the histogram of the underexposed image using custom function
     Mat stretchedImage;
-    underexposedImage.convertTo(stretchedImage, CV_8U, 255.0 / (maxVal - minVal), -minVal * 255.0 / (maxVal - minVal));
+    stretchHistogram(underexposedImage, stretchedImage, minVal, maxVal);
 
     // Save the stretched image
     imwrite("underexposedStretched.jpg", stretchedImage);
 
-    // Apply histogram equalization to the stretched image
+    // Apply histogram equalization to the stretched image using custom function
     Mat equalizedImage;
-    equalizeHist(stretchedImage, equalizedImage);
+    equalizeHistogram(stretchedImage, equalizedImage);
 
     // Save the equalized image
     imwrite("underexposedEqualized.jpg", equalizedImage);
-
-    // Display the boy image and its histogram
-    imshow("Boy Image", boyImage);
-    imshow("Histogram of Boy Image", boyHistImage);
 
     // Display the original, stretched, and equalized underexposed images
     imshow("Original Underexposed Image", underexposedImage);
@@ -99,4 +76,40 @@ int main(void) {
 
     waitKey(0);
     return 0;
+}
+
+// Implementation des fonctions personnalisées
+void calculateHistogram(const Mat& image, Mat& hist, int histSize) {
+    hist = Mat::zeros(1, histSize, CV_32F);
+    for (int y = 0; y < image.rows; y++) {
+        for (int x = 0; x < image.cols; x++) {
+            int pixelValue = (int)image.at<uchar>(y, x);
+            hist.at<float>(pixelValue)++;
+        }
+    }
+}
+
+void stretchHistogram(const Mat& image, Mat& stretchedImage, double minVal, double maxVal) {
+    stretchedImage = Mat::zeros(image.size(), image.type());
+    for (int y = 0; y < image.rows; y++) {
+        for (int x = 0; x < image.cols; x++) {
+            stretchedImage.at<uchar>(y, x) = saturate_cast<uchar>(255.0 * (image.at<uchar>(y, x) - minVal) / (maxVal - minVal));
+        }
+    }
+}
+
+void equalizeHistogram(const Mat& image, Mat& equalizedImage) {
+    Mat hist;
+    calculateHistogram(image, hist, 256);
+    Mat cdf = hist.clone();
+    for (int i = 1; i < 256; i++) {
+        cdf.at<float>(i) += cdf.at<float>(i - 1);
+    }
+    cdf *= 255.0 / image.total();
+    equalizedImage = Mat::zeros(image.size(), image.type());
+    for (int y = 0; y < image.rows; y++) {
+        for (int x = 0; x < image.cols; x++) {
+            equalizedImage.at<uchar>(y, x) = saturate_cast<uchar>(cdf.at<float>(image.at<uchar>(y, x)));
+        }
+    }
 }
